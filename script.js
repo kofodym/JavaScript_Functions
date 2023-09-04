76988beecfe5e8114be23199e419eb3d73ba66b3
KofoCole <kofodym07@yahoo.com>
2023-09-04 13:44:19 +0100

    Declare String of numbers and initialize the characters

---
+++ script.js
@@ -0,0 +1,13 @@
+// QUESTION 1:
+//You are presented with a string with a collection of data which are mixed alphabets i.e., "abdsgyhidsaadgbdaasbvcydsaabdgsaayre", create a function that will count through the string and show the number of a, b, d and y each contained in the string.
+// Note: the output is expected to be a number for each of the counted elements.
+
+
+//1. Declare String of numbers
+let stringLetters = "abdsgyhidsaadgbdaasbvcydsaabdgsaayre";
+// Initialize counts for 'a', 'b', 'd', and 'y' to 0
+let string_a = 0;
+let string_b = 0;
+let string_d = 0;
+let string_y = 0;
+let count;
